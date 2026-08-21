6c20c8a5f4f9d3b766f4310beac1698eac38cacd
Housinsects <tu_correo@ejemplo.com>
2026-08-21 00:00:30 -0500

    CAMBIO

--- scripts/importar_csv_python.py
+++ scripts/importar_csv_python.py
@@ -1,7 +1,7 @@
 import os
 import psycopg2
 
-DATABASE_URL = "postgresql://neondb_owner:npg_eQqKZ3JkD4nz@ep-damp-hat-afgjqwk2.us-west-2.aws.neon.tech/neondb?sslmode=require"
+postgresql://neondb_owner:npg_eQqKZ3JkD4nz@ep-damp-hat-afgjqwk2.c-2.us-west-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require
 
 def conectar_bd():
     print(f"🔍 Intentando conectar con esta URL exacta: {DATABASE_URL}")
