6edc539fc423703c4b63a173ac378ebec4924419
Housinsects <tu_correo@ejemplo.com>
2026-08-20 23:16:55 -0500

    change

--- scripts/importar_csv_python.py
+++ scripts/importar_csv_python.py
@@ -5,6 +5,7 @@ import psycopg2
 
 # Conexión directa con la contraseña nueva y URL limpia
 DATABASE_URL = "postgresql://neondb_owner:TU_NUEVA_CONTRASENA@ep-damp-hat-afgjqwk2.us-west-2.aws.neon.tech/neondb?sslmode=require"
+
 def importar_datos():
     print("⏳ Conectando a la base de datos de Neon...")
     try:
