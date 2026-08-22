a6e9510cf64aa4d834a6229fc21a0bcc184a7249
Housinsects <tu_correo@ejemplo.com>
2026-08-21 23:00:45 -0500

    change

---
+++ lib/data/catalogue-fallback.ts
@@ -0,0 +1,21 @@
+export const FALLBACK_CATALOGUE = [
+  {
+    id: "dried-specimens",
+    name: "Especímenes secos biológicos",
+    specimensCount: 150,
+    regions: [
+      {
+        id: "neotropical",
+        name: "Central South America Neotropics",
+        categories: [
+          {
+            id: "butterflies-lepidoptera-diurne",
+            slug: "butterflies-lepidoptera-diurne",
+            name: "Butterflies (Lepidoptera) Diurne",
+            specimensCount: 85
+          }
+        ]
+      }
+    ]
+  }
+];
