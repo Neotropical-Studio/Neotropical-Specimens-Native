9915568c9e5afc1ca3f73d49562178844f119834
Housinsects <tu_correo@ejemplo.com>
2026-08-22 07:19:53 -0500

    change

--- lib/sanity/client.ts
+++
@@ -1,7 +0,0 @@
-export const sanity = {
-  fetch: async <T = unknown>(_query: string, _params?: Record<string, unknown>): Promise<T> => {
-    return [] as T;
-  },
-};
-
-export const sanityPreview = sanity;
